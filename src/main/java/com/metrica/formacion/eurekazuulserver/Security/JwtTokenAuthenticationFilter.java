c600485b9c6e2e68119b89d3da72f6513a9ad03e
artfern95 <artfer050609>
2019-12-04 17:16:20 +0100

    bloqueando rutas de las apis

--- src/main/java/com/metrica/formacion/eurekazuulserver/Security/JwtTokenAuthenticationFilter.java
+++ src/main/java/com/metrica/formacion/eurekazuulserver/Security/JwtTokenAuthenticationFilter.java
@@ -1,4 +1,4 @@
-package com.metrica.formacion.eurekazuulserver.security;
+package com.metrica.formacion.eurekazuulserver.Security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
--- src/main/java/com/metrica/formacion/eurekazuulserver/Security/SecurityTokenConfig.java
+++ src/main/java/com/metrica/formacion/eurekazuulserver/Security/SecurityTokenConfig.java
@@ -1,13 +1,10 @@
-package com.metrica.formacion.eurekazuulserver.security;
+package com.metrica.formacion.eurekazuulserver.Security;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletResponse;
